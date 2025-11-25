ff3a53b2899178b40fbaf61c080c4db14e188446
Filip de Meyere <fdemeyere>
2025-11-25 10:08:46 +0100

    Let executeOrders run on separate thread

--- main.cpp
+++ main.cpp
@@ -5,6 +5,7 @@
 #include <chrono>
 #include <thread>
 #include <queue>
+#include <condition_variable>
 
 struct Order {
     char type;
@@ -14,6 +15,16 @@ struct Order {
 struct OrderQueue {
     std::queue<Order> queue;
     std::mutex mutex;
+    std::condition_variable cv;
+};
+
+struct TradingState {
+    std::mutex mutex;
+    double last_price;
+    double bal;
+    int pos;
+
+    TradingState(const double last_price, const double bal, const int pos) : last_price(last_price), bal(bal), pos(pos) {}
 };
 
 bool parseOrder(const std::string& cmd, char& c, int& qty) {
@@ -48,19 +59,21 @@ void placeOrders(OrderQueue& orders) {
             continue;
         }
         Order ord = {c, qty};
-        std::lock_guard<std::mutex> lock(orders.mutex);
-        orders.queue.push(ord);
+        {
+            std::lock_guard<std::mutex> lock(orders.mutex);
+            orders.queue.push(ord);
+        }
+        orders.cv.notify_one();
     }
 }
 
-void executeOrders(OrderQueue& orders, double& balance, int& pos, const double last_price) {
+void executeOrders(OrderQueue& orders, TradingState& trading_state) {
     while (true) {
         Order ord;
         {
-            std::lock_guard<std::mutex> lock(orders.mutex);
-            if (orders.queue.empty()) {
-                break;
-            }
+            std::unique_lock<std::mutex> lock(orders.mutex);
+            orders.cv.wait(lock, [&orders] { return !orders.queue.empty(); });
+
             ord = orders.queue.front();
             orders.queue.pop();
         }
@@ -68,33 +81,35 @@ void executeOrders(OrderQueue& orders, double& balance, int& pos, const double l
         const char type = ord.type;
         const int qty = ord.qty;
 
-        if ( type == 'b' && balance >= last_price * qty) {
-            balance -= last_price * qty;
-            pos += qty;
-        } else if (type == 's' && pos >= qty) {
-            balance += last_price * qty;
-            pos -= qty;
+        std::lock_guard<std::mutex> lock(trading_state.mutex);
+        if ( type == 'b' && trading_state.bal >= trading_state.last_price * qty) {
+            trading_state.bal -= trading_state.last_price * qty;
+            trading_state.pos += qty;
+        } else if (type == 's' && trading_state.pos >= qty) {
+            trading_state.bal += trading_state.last_price * qty;
+            trading_state.pos -= qty;
         } else {
             std::cout << "Not enough cash or shares\n";
         }
 
-        std::cout << "bal: " << balance << "\n" << "pos: " << pos << "\n";
+        std::cout << "bal: " << trading_state.bal << "\n" << "pos: " << trading_state.pos << "\n";
     }
 }
 
 int main() {
-    double bal = 1000;
-    int pos = 0;
+    TradingState trading_state(-1, 1000, 0);
 
     OrderQueue orders;
 
     std::string last_price;
     bool active_trading = false;
 
-    std::thread trading_thread;
+    std::thread consumer_thread;
+    std::thread producer_thread;
 
     std::string line;
-    std::ifstream read_file("../HistoricalData_1763459211275.csv");
+    // std::ifstream read_file("../HistoricalData_1763459211275.csv"); // Apple
+    std::ifstream read_file("../HistoricalData_1763676611258.csv"); // Amazon
     std::ofstream write_file("../feed.txt");
 
     getline(read_file, line);
@@ -110,21 +125,28 @@ int main() {
         std::getline(ss, date, comma);
         std::getline(ss, last_price, comma);
 
-        last_price = last_price.substr(1);
+        {
+            std::lock_guard<std::mutex> lock(trading_state.mutex);
+            trading_state.last_price = std::stod( last_price.substr(1));
+        }
 
         if (!active_trading) {
-            trading_thread = std::thread(placeOrders, std::ref(orders));
+            consumer_thread = std::thread(executeOrders, std::ref(orders), std::ref(trading_state));
+            producer_thread = std::thread(placeOrders, std::ref(orders));
             active_trading = true;
         }
-        std::this_thread::sleep_for(std::chrono::seconds(1));
-        executeOrders(orders, bal, pos, std::stod(last_price));
-        write_file << tick << comma << last_price << comma << pos << comma << bal << "\n" << std::flush;
+
+        std::this_thread::sleep_for(std::chrono::milliseconds(1000));
+
+        std::lock_guard<std::mutex> lock(trading_state.mutex);
+        write_file << tick << comma << trading_state.last_price << comma << trading_state.pos << comma << trading_state.bal << "\n" << std::flush;
         tick++;
     }
     write_file.close();
     read_file.close();
 
-    trading_thread.join();
+    producer_thread.join();
+    consumer_thread.join();
 
     return 0;
 }
